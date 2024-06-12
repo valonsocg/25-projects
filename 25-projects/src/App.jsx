e84647730a1840cc99c90e06feffc87733110d32
Alonso <valonsocg@gmail.com>
2024-06-12 16:05:12 -0500

    Added tree view component/navigation menu

--- 25-projects/src/App.jsx
+++ 25-projects/src/App.jsx
@@ -4,6 +4,8 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-mode-data";
 import RandomColor from "./components/random-color";
 import { StarRating } from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
 	return (
@@ -21,7 +23,10 @@ function App() {
 			{/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
 
 			{/* Load More Data Component */}
-			<LoadMoreData />
+			{/* <LoadMoreData /> */}
+
+			{/* Tree View Component/ Navigation Menu */}
+			<TreeView menus={menus} />
 		</>
 	);
 }
